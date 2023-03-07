976021ce219e39d6a80f3cc0073556259e18a756
RGPro <rami.rgprogrammer@gmail.com>
2023-03-07 22:20:14 +0100

    fonctionality fo rretrieving Ram usage and CPU load and updating the Monitor Widget

--- app/src/main/java/com/rgpro/widgetexample/widgets/Monitor.kt
+++ app/src/main/java/com/rgpro/widgetexample/widgets/Monitor.kt
@@ -1,5 +1,6 @@
 package com.rgpro.widgetexample.widgets
 
+import android.app.ActivityManager
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
@@ -7,9 +8,18 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
+import android.os.CpuUsageInfo
+import android.os.Debug.getMemoryInfo
+import android.os.HardwarePropertiesManager
+import android.os.Parcel
+import android.os.Parcelable.PARCELABLE_WRITE_RETURN_VALUE
 import android.util.Log
 import android.widget.RemoteViews
+import androidx.core.app.ActivityManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.rgpro.widgetexample.R
+import java.io.FileDescriptor
+
 
 //TODO implement a service(SystemResourceMonitorService) that register the batteryMonitor broadcast and update widget
 class Monitor : AppWidgetProvider() {
@@ -17,16 +27,22 @@ class Monitor : AppWidgetProvider() {
         public val TAG :String = Monitor.javaClass.name
     }
     private var componentName: ComponentName? = null
+    private lateinit var context : Context
     private var isCharging = false
     private var batteryLevelInPercentage = 0
+    private var memoryUsageInPercentage = 0
+    private var cpuLoadInPercentage = 0.0 ;
+    private var cpuInfoError = false
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray //the user can choose create multiple instances of the current widget
         //the app should update all the instance
     ) {
-        Log.d(TAG,"updating all monitor widgets")
-        // There may be multiple widgets(instances) active, so update all of them
+
+        getCPULoad(context)
+        getRamUsage(context)
+
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
@@ -34,12 +50,15 @@ class Monitor : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         this.componentName = ComponentName(context, this.javaClass)
+        this.context = context
+        //battery info first retrieval
         val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         context.applicationContext.registerReceiver(this,filter).also {
             extractBatteryStatusFromIntent(it)
         }
 
-        //TODO get CPU and RAm usage
+        getCPULoad(context)
+        getRamUsage(context)
     }
 
     override fun onDisabled(context: Context) {
@@ -51,9 +70,7 @@ class Monitor : AppWidgetProvider() {
     override fun onReceive(context: Context?, intent: Intent?) {
 
         val action = intent!!.action
-        Log.d(TAG,"received ACTION $action")
         if (Intent.ACTION_BATTERY_CHANGED == action) {
-            Log.d(TAG,"received battery changed prodcast")
            extractBatteryStatusFromIntent(intent)
             val widgetManager = AppWidgetManager.getInstance(context)
             val ids = widgetManager.getAppWidgetIds(componentName)
@@ -75,6 +92,7 @@ class Monitor : AppWidgetProvider() {
     ) {
         // Construct the RemoteViews object
         val views = RemoteViews(context.packageName, R.layout.monitor).also {
+            //Battery info
             it.setTextViewText(R.id.batteryPercentageText, "${batteryLevelInPercentage}%")
             it.setProgressBar(R.id.batteryPercentageBar, 100, batteryLevelInPercentage, false)
             it.setImageViewResource(
@@ -85,6 +103,19 @@ class Monitor : AppWidgetProvider() {
                     R.drawable.ic_battery_good
                 }
             )
+            //CPU info
+            if(!cpuInfoError) {
+                it.setTextViewText(R.id.cpuUsageText, "$cpuLoadInPercentage %")
+                it.setProgressBar(R.id.cpuUsageBar, 100, cpuLoadInPercentage.toInt(), false);
+            }else{
+                it.setTextViewText(R.id.cpuUsageText, "Error")
+                it.setProgressBar(R.id.cpuUsageBar, 100, 0, false);
+            }
+            it.setImageViewResource(R.id.cpuIcon,R.drawable.ic_cpu)
+            //Ram info
+            it.setTextViewText(R.id.ramUsageText,"$memoryUsageInPercentage %")
+            it.setProgressBar(R.id.ramUsageBar,100,memoryUsageInPercentage,false);
+            it.setImageViewResource(R.id.ramIcon,R.drawable.ic_ram)
 
         }
         appWidgetManager.updateAppWidget(appWidgetId, views)
@@ -98,6 +129,49 @@ class Monitor : AppWidgetProvider() {
             val level: Int = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
             val scale: Int = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
             batteryLevelInPercentage = (level * 100 / scale.toFloat()).toInt()
+
+        }
+    }
+    private fun getCPULoad(context : Context)
+    {
+        //TODO get CPU load
+        val hwPropertiesManager=
+            context.getSystemService(Context.HARDWARE_PROPERTIES_SERVICE) as HardwarePropertiesManager
+        try {
+            val cpuUsages = hwPropertiesManager.cpuUsages
+            cpuUsages.let { infoCPUs ->
+                //var descriptor
+                for (cpu in infoCPUs) {//for (cpu : CpuUsageInfo in infoCPUs){
+                    if (cpu.describeContents() == CpuUsageInfo.CONTENTS_FILE_DESCRIPTOR) {
+                        val parcel = Parcel.obtain();
+                        cpu.writeToParcel(parcel, PARCELABLE_WRITE_RETURN_VALUE)
+                        val fileDescriptor = parcel.readFileDescriptor();
+                        Log.d(TAG, "file descriptor for cpu info $fileDescriptor")
+
+                        //TODO
+                    }
+                }
+            }
+        }catch (e: SecurityException){
+            cpuInfoError = true ;
+        }
+    }
+    private fun getRamUsage(context : Context)
+    {
+        //TODO get the ram Load
+        val activityManager =
+            context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.let { manager ->
+            val memInfo = ActivityManager.MemoryInfo()
+
+            // Fetching the data from the ActivityManager
+            manager.getMemoryInfo(memInfo)
+
+            // Fetching the available and total memory and converting into Giga Bytes
+            val availMemory  = memInfo.availMem.toDouble()
+            val totalMemory= memInfo.totalMem.toDouble()
+
+            memoryUsageInPercentage = ((1.0- (availMemory/totalMemory))*100).toInt() ;
 
         }
     }
