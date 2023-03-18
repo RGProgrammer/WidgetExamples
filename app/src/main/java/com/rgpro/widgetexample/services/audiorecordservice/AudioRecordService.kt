15f83a7bcef8470f4719d7349bc0f1f10b18bd2e
RGPro <rami.rgprogrammer@gmail.com>
2023-03-19 00:51:27 +0100

    initial working background audio recording with widget: start//stop recording and audio record service state update broadcast

--- app/src/main/java/com/rgpro/widgetexample/services/audiorecordservice/AudioRecordService.kt
+++ app/src/main/java/com/rgpro/widgetexample/services/audiorecordservice/AudioRecordService.kt
@@ -1,8 +1,11 @@
 package com.rgpro.widgetexample.services.audiorecordservice
 
+import android.Manifest
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.media.MediaRecorder
@@ -12,6 +15,9 @@ import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
+import com.rgpro.widgetexample.R
+import com.rgpro.widgetexample.widgets.AudioRecorder
 import java.io.IOException
 import java.lang.IllegalStateException
 import java.util.*
@@ -21,8 +27,10 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
         val TAG = AudioRecordService::class.simpleName
         const val ACTION_BROADCAST_AUDIO_RECORD_STATE_CHANGE =
             "com.rgpro.services.AudioRecordService.STATE_CHANGED_BROADCAST"
-        const val  EXTRA_RECORD_STATE =  "state"
-        const val  EXTRA_ENABLE_BROADCAST ="broadcast"
+        const val EXTRA_RECORD_STATE = "state"
+        const val EXTRA_ENABLE_BROADCAST = "broadcast"
+        const val EXTRA_CLASSNAME_BROADCAST_TARGET = "target"
+
         const val STATE_ERROR_RECORDING = 0
         const val STATE_STARTED_RECORDING = 1
         const val STATE_STOPPED_RECORDING = 2
@@ -31,13 +39,19 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
             get() = default_outDirectory
         val default_MaxDuration = 20000 // 20 seconds
 
-    }
-    inner class AudioRecordServiceBridge(val service: AudioRecordService) : Binder() {
-    }
-    interface OnRecordStatusChangedListener{
-        fun onStatusChanged(context : Context, state : Int, errorMsg : String? = null ) ;
+        private var _audioServiceInstance: AudioRecordService? = null
+        val serviceInstance
+            get() = _audioServiceInstance
     }
 
+    inner class AudioRecordServiceBridge(val service: AudioRecordService) : Binder() {
+    }
+
+    interface OnRecordStatusChangedListener {
+        fun onStatusChanged(context: Context, state: Int, errorMsg: String? = null);
+    }
+
+    private var targetComponent: ComponentName? = null
     private var sendStateChangeBroadcast = false
     private var recorder: MediaRecorder? = null
     private var binder = AudioRecordServiceBridge(this)
@@ -45,9 +59,8 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
     private var isRecording = false
 
     private var outDirectory: String? = null
-    private var maxDuration : Int? = default_MaxDuration
-
-    private var _onStatusChangedListener : OnRecordStatusChangedListener? =null
+    private var maxDuration: Int? = default_MaxDuration
+    private var _onStatusChangedListener: OnRecordStatusChangedListener? = null
     public var onStatusChangedListener: OnRecordStatusChangedListener?
         get() {
             return _onStatusChangedListener
@@ -62,18 +75,46 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
 
     override fun onDestroy() {
         super.onDestroy()
+        _audioServiceInstance = null
+        Log.d(TAG, "onDestroy: Service destroyed")
         recorder?.let {
-            if(isRecording)
+            if (isRecording)
                 it.stop()
             it.release();
         }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //TODO read extra parameter from Intent
-        intent?.let {
-            sendStateChangeBroadcast = it.getBooleanExtra(EXTRA_ENABLE_BROADCAST,false )
+        _audioServiceInstance = this
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "audio channel"
+            val descriptionText = "Audio record service "
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("audio_record", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            val notification = Notification.Builder(this, "audio_record")
+                .setContentTitle("Recorder")
+                .setContentText("Ready")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .build()
+
+            startForeground(515, notification)
         }
+        intent?.let {
+            sendStateChangeBroadcast = it.getBooleanExtra(EXTRA_ENABLE_BROADCAST, false)
+            it.getStringExtra(EXTRA_CLASSNAME_BROADCAST_TARGET)?.also { target ->
+                targetComponent = ComponentName(this, target)
+            }
+
+            Log.d(TAG, "onStartCommand: Service with broadcast state $sendStateChangeBroadcast")
+        }
+        Log.d(TAG, "onStartCommand: Service started")
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -109,7 +150,7 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
             Log.e(TAG, "error ${e.message}");
             recorder?.release();
             recorder = null;
-            onStatusChangedListener?.onStatusChanged(this,0,e.message) // error
+            onStatusChangedListener?.onStatusChanged(this, 0, e.message) // error
             return false;
         }
         return true;
@@ -117,7 +158,7 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
 
     public fun startRecording() {
         prepareNewRecorderInstance()
-        val outputDir = if (outDirectory!=null) {
+        val outputDir = if (outDirectory != null) {
             outDirectory
         } else {
             default_outDirectory
@@ -131,32 +172,44 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
             recorder?.prepare();
             recorder?.start();
             isRecording = true;
-            onStatusChangedListener?.onStatusChanged(this, STATE_STARTED_RECORDING,null);
-            if(sendStateChangeBroadcast) sendStateBroadcast(STATE_STARTED_RECORDING)
+            onStatusChangedListener?.onStatusChanged(this, STATE_STARTED_RECORDING, null);
+            if (sendStateChangeBroadcast) sendStateBroadcast(STATE_STARTED_RECORDING)
         } catch (e: IOException) {
             isRecording = false;
             Log.e(TAG, "startRecording: ", e)
-            onStatusChangedListener?.onStatusChanged(this, STATE_ERROR_RECORDING,e.message) // error
-            if(sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
+            onStatusChangedListener?.onStatusChanged(
+                this,
+                STATE_ERROR_RECORDING,
+                e.message
+            ) // error
+            if (sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
         } catch (e: IllegalStateException) {
             isRecording = false;
             Log.e(TAG, "startRecording: ", e)
-            onStatusChangedListener?.onStatusChanged(this,STATE_ERROR_RECORDING,e.message) // error
-            if(sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
+            onStatusChangedListener?.onStatusChanged(
+                this,
+                STATE_ERROR_RECORDING,
+                e.message
+            ) // error
+            if (sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
         }
 
     }
 
     public fun stopRecording() {
         recorder?.let {
-            if(isRecording) {
+            if (isRecording) {
                 try {
                     it.stop()
-                    onStatusChangedListener?.onStatusChanged(this, STATE_STOPPED_RECORDING,null);
-                    if(sendStateChangeBroadcast) sendStateBroadcast(STATE_STOPPED_RECORDING)
-                } catch (e:Exception) {
-                    onStatusChangedListener?.onStatusChanged(this, STATE_ERROR_RECORDING,e.message) // error
-                    if(sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
+                    onStatusChangedListener?.onStatusChanged(this, STATE_STOPPED_RECORDING, null);
+                    if (sendStateChangeBroadcast) sendStateBroadcast(STATE_STOPPED_RECORDING)
+                } catch (e: Exception) {
+                    onStatusChangedListener?.onStatusChanged(
+                        this,
+                        STATE_ERROR_RECORDING,
+                        e.message
+                    ) // error
+                    if (sendStateChangeBroadcast) sendStateBroadcast(STATE_ERROR_RECORDING)
                 }
 
             }
@@ -170,24 +223,32 @@ class AudioRecordService : Service(), MediaRecorder.OnInfoListener {
     public fun isRecording(): Boolean {
         return isRecording;
     }
-    public fun setOutputDirectory(directory :String) {
+
+    public fun setOutputDirectory(directory: String) {
         //TODO check if the directory is valid
-        outDirectory = directory ;
+        outDirectory = directory;
     }
-    public fun setMaxDuration(duration: Int){
-        Log.d(TAG,"max duration set to $duration")
-        maxDuration = duration ;
+
+    public fun setMaxDuration(duration: Int) {
+        Log.d(TAG, "max duration set to $duration")
+        maxDuration = duration;
     }
-    private fun sendStateBroadcast (state : Int ){
-        val broadcastIntent = Intent().also {
-            it.action = ACTION_BROADCAST_AUDIO_RECORD_STATE_CHANGE
-            it.putExtra(EXTRA_RECORD_STATE,state)
+
+    private fun sendStateBroadcast(state: Int) {
+        val broadcastIntent = Intent(ACTION_BROADCAST_AUDIO_RECORD_STATE_CHANGE).also {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                it.component = targetComponent
+            }
+            //it.component = ComponentName(this,AudioRecordService::class.java)
+            it.putExtra(EXTRA_RECORD_STATE, state)
         }
         sendBroadcast(broadcastIntent)
     }
+
     override fun onInfo(mr: MediaRecorder?, what: Int, extra: Int) {
-        if(what == MEDIA_RECORDER_INFO_MAX_DURATION_REACHED ||
-            what == MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED){
+        if (what == MEDIA_RECORDER_INFO_MAX_DURATION_REACHED ||
+            what == MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED
+        ) {
             this.stopRecording()
         }
     }
--- app/src/main/java/com/rgpro/widgetexample/widgets/AudioRecorder.kt
+++ app/src/main/java/com/rgpro/widgetexample/widgets/AudioRecorder.kt
@@ -40,14 +40,15 @@ class AudioRecorder : AppWidgetProvider() {
             if (receivedIntent.action == ACTION_AUDIO_SERVICE_CONTROL) {
                 Log.d(TAG, "onReceive: action = $ACTION_AUDIO_SERVICE_CONTROL")
                 val audioRecordServiceIntent = Intent(context, AudioRecordService::class.java)
-                (peekService(
-                    context,
-                    audioRecordServiceIntent
-                ) as? AudioRecordService.AudioRecordServiceBridge)?.also { iBinder ->
+                    .also {
+                        it.component = ComponentName(context, AudioRecordService::class.java)
+                    }
+                AudioRecordService.serviceInstance?.let { service ->
                     val action = receivedIntent.getIntExtra(EXTRA_CONTROl_AUDIO_RECORD, -1)
+                    Log.d(TAG, "onReceive: service action = $action")
                     when (action) {
-                        START_AUDIO_RECORD -> iBinder.service.startRecording()
-                        STOP_AUDIO_RECORD -> iBinder.service.stopRecording()
+                        START_AUDIO_RECORD -> service.startRecording()
+                        STOP_AUDIO_RECORD -> service.stopRecording()
                     }
                 }
                 this.onUpdate(
@@ -62,20 +63,15 @@ class AudioRecorder : AppWidgetProvider() {
                     AudioRecordService.EXTRA_RECORD_STATE,
                     AudioRecordService.STATE_ERROR_RECORDING
                 )
-                isRecording = when (state) {
-                    AudioRecordService.STATE_STARTED_RECORDING -> true
-                    AudioRecordService.STATE_STOPPED_RECORDING -> false
-                    else -> {
+                when (state) {
+                    AudioRecordService.STATE_ERROR_RECORDING -> {
                         Toast.makeText(
                             context,
                             "Error Audio Recording state",
                             Toast.LENGTH_LONG
                         ).show()
-                        false
                     }
                 }
-
-
                 // update all the widgets to show the correct data
                 this.onUpdate(
                     context,
@@ -93,7 +89,9 @@ class AudioRecorder : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-
+        AudioRecordService.serviceInstance?.let { service ->
+            isRecording = service.isRecording()
+        }
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
@@ -105,47 +103,43 @@ class AudioRecorder : AppWidgetProvider() {
             PermissionHelper.launchPermissionSettings(context)
         }
         val audioRecorderServiceIntent = Intent(context, AudioRecordService::class.java)
-            .putExtra(AudioRecordService.EXTRA_ENABLE_BROADCAST,true)
+            .putExtra(AudioRecordService.EXTRA_ENABLE_BROADCAST, true)
 
         (context.getSystemService(ALARM_SERVICE) as? AlarmManager)?.also { alarmManager ->
-
-                alarmManager.set(
-                    AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 500,
+            alarmManager.set(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 500,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    audioRecorderServiceIntent.putExtra(
+                        AudioRecordService.EXTRA_CLASSNAME_BROADCAST_TARGET,
+                        AudioRecorder::class.java.name
+                    )
+                    PendingIntent.getForegroundService(
+                        context,
+                        0,
+                        audioRecorderServiceIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                } else {
                     PendingIntent.getService(
                         context,
                         0,
                         audioRecorderServiceIntent,
-                        PendingIntent.FLAG_IMMUTABLE
+                        PendingIntent.FLAG_UPDATE_CURRENT
                     )
-                )
-            }
+                }
 
-//        SystemClock.sleep(1000)
-//        val binder = this.peekService(
-//            context,
-//            audioRecorderServiceIntent
-//        ) as? AudioRecordService.AudioRecordServiceBridge
-//        binder?.let {
-//            isRecording = it.service.isRecording()
-//            Toast.makeText(context,"Audio Service started",Toast.LENGTH_SHORT).show()
+            )
+        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            context.startForegroundService(audioRecorderServiceIntent)
+//        }else{
+//            context.startService(audioRecorderServiceIntent)
 //        }
-//        val widgetManager = AppWidgetManager.getInstance(context)
-//        val ids =
-//            widgetManager.getAppWidgetIds(ComponentName(context, this.javaClass))
-//        // update all the widgets to show the correct data
-//        this.onUpdate(
-//            context,
-//            widgetManager,
-//            ids
-//        )
-
     }
 
     override fun onDisabled(context: Context) {
         val audioRecorderServiceIntent = Intent(context, AudioRecordService::class.java)
         context.stopService(audioRecorderServiceIntent)
-        context.registerReceiver(this, IntentFilter(ACTION_AUDIO_SERVICE_CONTROL))
-        context.registerReceiver(this, IntentFilter(ACTION_BROADCAST_AUDIO_RECORD_STATE_CHANGE))
         Toast.makeText(context, "Disabled record service", Toast.LENGTH_LONG).show()
     }
 
@@ -156,17 +150,36 @@ class AudioRecorder : AppWidgetProvider() {
     ) {
         // Construct the RemoteViews object
         val views = RemoteViews(context.packageName, R.layout.audio_recorder)
-        val broadcastIntent = Intent(ACTION_AUDIO_SERVICE_CONTROL)
-        broadcastIntent.flags = Intent.FLAG_FROM_BACKGROUND
+        val broadcastIntent = Intent(ACTION_AUDIO_SERVICE_CONTROL).also {
+            val cn = ComponentName(context, AudioRecorder::class.java)
+            it.setComponent(cn)
+        }
+
         if (!isRecording) {
             views.setInt(R.id.recorder_button, "setBackgroundResource", R.drawable.ic_record_button)
             broadcastIntent.putExtra(EXTRA_CONTROl_AUDIO_RECORD, START_AUDIO_RECORD)
-            views.setOnClickPendingIntent(R.id.recorder_button, PendingIntent.getBroadcast(context,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT))
+            views.setOnClickPendingIntent(
+                R.id.recorder_button,
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    broadcastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            )
 
         } else {
             views.setInt(R.id.recorder_button, "setBackgroundResource", R.drawable.ic_stop_button)
             broadcastIntent.putExtra(EXTRA_CONTROl_AUDIO_RECORD, STOP_AUDIO_RECORD)
-            views.setOnClickPendingIntent(R.id.recorder_button, PendingIntent.getBroadcast(context,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT))
+            views.setOnClickPendingIntent(
+                R.id.recorder_button,
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    broadcastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            )
         }
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views)
